fb0a5796652a7a06c03f4b7b72ee41bff1cadb58
Julien PAUSE <julien.pause@epitech.eu>
2022-02-13 16:54:17 +0400

    feat: add choose categories

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Button, Stack, Container, Typography, Box } from '@mui/material';
+import { Button, Stack, Container, Typography, Box, Dialog, DialogTitle } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import raining_money from '../assets/raining_money.gif'
 import logo from '../assets/logo.png'
 import LanguageSwitcher from '../components/LanguageSwitcher';
+import ChooseCategoriesDialog from '../components/ChooseCategories';
+import compareCategories from '../data/index'
+import { useTranslation } from 'react-i18next';
 
 export default function LandingPage() {
+  const { t } = useTranslation('landingPage')
+  const [open, setOpen] = useState(false)
+  const [checked, setChecked] = useState<string[]>(compareCategories.map(category =>
+    category.title
+  ))
+
+  const onClose = () => {
+    setOpen(false)
+  }
   const navigate = useNavigate()
 
   return (
@@ -14,16 +26,22 @@ export default function LandingPage() {
       <Container component="main" maxWidth="md" sx={{ mb: 10 }} >
         <Stack spacing={4} sx={{ alignItems: "center" }}>
           <img src={logo} alt="Most successfull logo" style={{ width: 250, height: 250 }}/>
-          <Typography variant="h2" component="h1" color="white">
-            Most successfull
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom color="white" maxWidth="sm" textAlign="center">
-            Can you guess who has the most success ?
+          <Typography variant="h5" component="h2" gutterBottom color="white"  textAlign="center">
+            { t('caption') }
           </Typography>
           <Button variant="contained" size="large" endIcon={<NavigateNextIcon />} onClick={() => navigate('most-success')}>
-            Start playing
+            { t('start_playing') }
+          </Button>
+          <Button variant="contained" size="large"  onClick={() => setOpen(true)}>
+            { t('choose_categories') }
           </Button>
         </Stack>
+        <ChooseCategoriesDialog
+          open={open}
+          onClose={onClose}
+          checked={checked}
+          setChecked={setChecked}
+        />
       </Container>
       <LanguageSwitcher />
     </Box>
